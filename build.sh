2522a0c518399d43f45d742471fc74576c8ada8c
Sarvesh Dubey <sarvesh.dubey@cdk.com>
2017-03-14 23:38:18 +0530

    removing sonar

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd $TRAVIS_BUILD_DIR/secret-santa
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package
