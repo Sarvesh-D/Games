16d57b42e7dc8c9fa6ddedabb681a8ea4802f6c6
Sarvesh Dubey <sarvesh.dubey@cdk.com>
2017-03-14 23:14:32 +0530

    testing sonar token

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd $TRAVIS_BUILD_DIR/secret-santa
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -Dsonar.login=${SONAR_TOKEN}
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar
